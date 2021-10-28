22f9632be6a3710f07ba32acb564b5d1e6450905
xaopaibun <vanquy33338888@gmail.com>
2021-10-28 22:06:45 +0700

    first commit

---
+++ HeChuyenGia_Nhom2/HeChuyenGia_Nhom16/Properties/Settings.Designer.cs
@@ -0,0 +1,4 @@
+Ò¦sBÃÖñ¦Ò‹»MaBÃÖñ¦Òñ–{Úù‹‹‹qBÃ-ÀB©¦©‹qBDÄ©ÏBÃÖñ¦ÒsBÃÖñ¦Òñ–{Úù‹‹‹qBÃ-ÀÀÀÊ±}ñ±9¥Öl5Öñ¦Òñ–{ÚùI:¥Î)[{ðÖñÏssS±Öñðq‹‹qBÃ-ÀÀÀÊ)[{‹‹qBÃ-ÀB©xÖñðÖñÏssS±Ö{¥ÖvÛ¥ÖvÙÒÖñ{‹Öñð––{Ú–§{û°¥ÖñxBñ–x»Öñ‹B©xûI)[{ðÖñÏBÃÖñ¦Òïa‹‹q‹‹‹‹‹D”Ö3aì95ñÖñ–³:¥ÖñÖÖ‹‹‹eÒ)[{‹‹qBÃ-ÀB©1\Ò¦ÖÖXÖñ\‹‹‹ìS±!qíqÞ§K±çBÃÖS±!È\\n{eÒ)X
+Öñ!rq‹‹‹‹îðÖ‹‹qBÃ-ÀB13±±:–¦¦ÒsBÃÖñ¦{ÚqÖì\Ö‹‹qBÃ-ÀÊž±–{qdïa/)[{95\MS±çBÃÖSX95©‹)[{‹–x±±¥\ÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÀÊ±}ñ–ŒÖv{95©‹)[{‹–x)[{ð–\=sÃñBDÄ©î:¥ÖñÖÖñq‹‹qBÃ-ÀB©‹qS±¥ÖvÒïa/)[{95\À\Ò)[{‹‹qBÃ-ÀÊž±–{qdïa/)[{95\M8ïçBÃÖS±!È\\n{ñÖ‹‹‹e–„}q»Ú8”ÙÒïa/¦{ÚV‹±}{)q‹‹‹’àDÖM±¥‹‹‹DÞ§KÚØÄÖñxÖ¥‹‹‹‹±Ä±1Ú1ÒïaìBÃ)[{Ò)XBÃÖñ¦Ò‹u{Ú!IØ‹‹q»Ú8”ÖÀˆ)ÖÖX§ñ––aÖÖÖq{Ò¦’sBÃÖñ¦{Ú¥‹‹‹B©x){‹‹qÊç)ÖÖ9ØÚïa‹‹qBÃ-ÀÀÀÊ±}{ûsÞ€71\Ò¦ÖÖXÖñÖñðNR{ì–ÖÒÖ*\Ö*\çà){‹‹qBÃ-ÀB©x±)[{Ò)XBÃÖñ¦Ò‹¥ÖvÒïa/)[{95\MYñ–ÖvÛ¥ÖñxÖ¥Ö»–—
+5\–{S±À\Ò)¥ÖñxÖñ–„ÖX)[{)XH-¥Öñ–³:¥ÖñÖM-}BÃÖc‹‹BÒsBÃÖñ¦Òñ–{Úù‹‹‹qBÃXÀsBÃÖñð95©‹)[{‹–xÄ±ô-MXÖñ\ÖHá¤ñ–5ïÁ{qÒïa‹‹qBÃ-ÀB©°ÒsBÃÖñ¦Ò)‹zT¥Öñ–„ÖXìpñ–³:¥ÖñÖñðNÖI))S–âØ–x)[{)X
+±?{ïçB
